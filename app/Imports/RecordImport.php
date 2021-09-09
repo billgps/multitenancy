c91e8047cec919e06865d5ccc019c13dfbbdc8e8
ckaryuusai <kahfi@globalpromedika.co.id>
2021-09-09 15:48:28 +0700

    debug record import

--- app/Imports/RecordImport.php
+++ app/Imports/RecordImport.php
@@ -17,28 +17,31 @@ class RecordImport implements ToModel, WithHeadingRow, WithValidation
     */
     public function model(array $row)
     {
-        return new Record([
-            'id'    => $row['id'], 
-            'cal_date'     => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal']),
-            'label'    => $row['no_label'], 
-            'calibration_status'     => $row['status_kalibrasi'],
-            'vendor'    => 'PT Global Promedika Service',
-            'certificate'     => null,
-            'report'    => null, 
-            'result'     => $row['hasil_kalibrasi'],
-            'inventory_id'    => $row['inventory_id'],
-        ]);
+        try {
+            return new Record([
+                'id'    => $row['id'], 
+                'cal_date'     => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal']),
+                'label'    => $row['no_label'], 
+                'calibration_status'     => $row['status_kalibrasi'],
+                'vendor'    => 'PT Global Promedika Service',
+                'certificate'     => null,
+                'report'    => null, 
+                'result'     => $row['hasil_kalibrasi'],
+                'inventory_id'    => $row['inventory_id'],
+            ]);
+        } catch (\Throwable $th) {
+            dd($th);
+        }
     }
 
     public function rules(): array
     {
-        return [];
-        // return [
-        //     '*.tanggal' => ['required'],
-        //     '*.no_label' => ['required', 'max:255'],
-        //     '*.status_kalibrasi' => ['required', 'max:255'],
-        //     '*.hasil_kalibrasi' => ['required', 'max:255'],
-        //     '*.inventory_id' => ['required', 'integer']
-        // ];
+        return [
+            '*.tanggal' => ['required'],
+            '*.no_label' => ['required', 'max:255'],
+            '*.status_kalibrasi' => ['required', 'max:255'],
+            '*.hasil_kalibrasi' => ['required', 'max:255'],
+            '*.inventory_id' => ['required', 'integer']
+        ];
     }
 }
