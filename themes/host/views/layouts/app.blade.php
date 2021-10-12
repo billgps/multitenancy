db7a6a4c53ca996dbaa2beba7dc402b9f1861bbb
ckaryuusai <kahfi@globalpromedika.co.id>
2021-10-12 13:43:06 +0700

    add sticky admin sidebar

--- themes/host/views/layouts/app.blade.php
+++ themes/host/views/layouts/app.blade.php
@@ -71,97 +71,89 @@
         </div>
     </header>
     <div id="app" class="flex w-full h-full overflow-auto">
-        <div class="h-full">
-            <div id="sideBar" class="w-64 h-full sticky bg-gray-800" :class="{'is-close': isClose, 'hidden': isClose, 'w-60': !isClose}">
-                {{-- <div class="flex items-center justify-center">
-                    <img class="h-6" src="https://premium-tailwindcomponents.netlify.app/assets/svg/tailwindcomponent-light.svg">
-                </div> --}}
-    
-                <nav class="pt-20">
-                    <div>
-                        <a href="{{ route('administrator.dashboard') }}" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
-                            <span class="flex items-center">
-                                <i class="fas fa-home"></i>
-                                <span class="mx-4">Dashboard</span>
-                            </span>
+        <aside id="sideBar" class="flex flex-col sticky top-0 w-64 h-screen bg-gray-800" :class="{'is-close': isClose, 'hidden': isClose, 'w-60': !isClose}">
+            <nav class="pt-12 text-sm">
+                <div>
+                    <a href="{{ route('administrator.dashboard') }}" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
+                        <span class="flex items-center">
+                            <i class="fas fa-home"></i>
+                            <span class="mx-4">Dashboard</span>
+                        </span>
+                    </a>
+                </div>
+                <div x-data="{ open: false }">
+                    <button @click="open = !open" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
+                        <span class="flex items-center">
+                            <i class="fas fa-users"></i>
+                            <span class="mx-4">Tenants</span>
+                        </span>
+
+                        <span>
+                            <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
+                                <path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </span>
+                    </button>
+
+                    <div x-show="open" class="bg-gray-700">
+                        <a class="py-2 px-14 block text-sm text-gray-100 hover:bg-gray-600 hover:text-white" href="{{ route('tenant.create') }}">
+                            <i class="fas fa-plus mr-2"></i>
+                            New Tenant
                         </a>
                     </div>
-                    <div x-data="{ open: false }">
-                        <button @click="open = !open" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
-                            <span class="flex items-center">
-                                <i class="fas fa-users"></i>
-                                <span class="mx-4">Tenants</span>
-                            </span>
-    
-                            <span>
-                                <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
-                                    <path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                            </span>
-                        </button>
-    
-                        <div x-show="open" class="bg-gray-700">
-                            <a class="py-2 px-16 block text-sm text-gray-100 hover:bg-gray-600 hover:text-white" href="{{ route('tenant.create') }}">
-                                <i class="fas fa-plus mr-2"></i>
-                                New Tenant
-                            </a>
-                        </div>
+                </div>
+                {{-- <div x-data="{ open: false }">
+                    <button @click="open = !open" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
+                        <span class="flex items-center">
+                            <i class="fas fa-balance-scale"></i>
+                            <span class="mx-4">Vendor</span>
+                        </span>
+
+                        <span>
+                            <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
+                                <path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </span>
+                    </button>
+
+                    <div x-show="open" class="bg-gray-700">
+                        <a class="py-2 px-16 block text-sm text-gray-100 hover:bg-gray-600 hover:text-white" href="{{ route('vendor.create') }}">
+                            <i class="fas fa-plus mr-2"></i>
+                            New Vendor
+                        </a>
+                        <a class="py-2 px-16 block text-sm text-gray-100 hover:bg-gray-600 hover:text-white" href="{{ route('vendor.index') }}">
+                            <i class="fas fa-list mr-2"></i>
+                            Vendor List
+                        </a>
                     </div>
-                    {{-- <div x-data="{ open: false }">
-                        <button @click="open = !open" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
-                            <span class="flex items-center">
-                                <i class="fas fa-balance-scale"></i>
-                                <span class="mx-4">Vendor</span>
-                            </span>
-    
-                            <span>
-                                <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
-                                    <path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                            </span>
-                        </button>
-    
-                        <div x-show="open" class="bg-gray-700">
-                            <a class="py-2 px-16 block text-sm text-gray-100 hover:bg-gray-600 hover:text-white" href="{{ route('vendor.create') }}">
-                                <i class="fas fa-plus mr-2"></i>
-                                New Vendor
-                            </a>
-                            <a class="py-2 px-16 block text-sm text-gray-100 hover:bg-gray-600 hover:text-white" href="{{ route('vendor.index') }}">
-                                <i class="fas fa-list mr-2"></i>
-                                Vendor List
-                            </a>
-                        </div>
-                    </div> --}}
-                    <div>
-                        <form method="post" action="{{ route('administrator.logout') }}" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
-                            @csrf
-                            <span id="logoutBtn" class="flex items-center">
-                                <i class="fas fa-power-off"></i>
-                                <span class="mx-4">Logout</span>
-                            </span>
-                            <script>
-                                $('#logoutBtn').on('click', function(e) {
-                                    e.preventDefault();
-                                    $(this).closest('form').submit();
-                                });
-                            </script>
-                        </form>
-                    </div>
-                </nav>
-    
-                {{-- <div class="absolute flex justify-center bottom-0 my-8 text-xs text-gray-100">
-                    <span class="mx-auto">
-                        Copyright Global Promedika Service 2021
-                    </span>
-                    <a class="flex items-center py-2 px-8 text-gray-100 hover:text-gray-200" href="#">
-                        <img class="h-6 w-6 rounded-full mr-3 object-cover" src="https://lh3.googleusercontent.com/a-/AOh14Gi0DgItGDTATTFV6lPiVrqtja6RZ_qrY91zg42o-g" alt="avatar">
-                        <span>Username</span>
-                    </a>
                 </div> --}}
-            </div>
-        </div>
+                <div>
+                    <form method="post" action="{{ route('administrator.logout') }}" class="w-full flex justify-between items-center py-3 px-6 text-gray-100 cursor-pointer hover:bg-gray-700 hover:text-gray-100 focus:outline-none">
+                        @csrf
+                        <span id="logoutBtn" class="flex items-center">
+                            <i class="fas fa-power-off"></i>
+                            <span class="mx-4">Logout</span>
+                        </span>
+                        <script>
+                            $('#logoutBtn').on('click', function(e) {
+                                e.preventDefault();
+                                $(this).closest('form').submit();
+                            });
+                        </script>
+                    </form>
+                </div>
+            </nav>
+
+            {{-- <div class="flex justify-center mt-auto mb-6 text-xs text-gray-100">
+                <span class="mx-aut text-center">
+                    Provided by 
+                    <br>
+                    Global Promedika Services
+                </span>
+            </div> --}}
+        </aside>
         @yield('content')
     </div>
 </body>
