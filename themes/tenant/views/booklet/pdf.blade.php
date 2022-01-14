c42348e2368973942fd2b4295f2e5cfd2270b630
ckaryuusai <kahfi@globalpromedika.co.id>
2022-01-14 16:19:55 +0700

    add calibration result on pdf

--- themes/tenant/views/booklet/pdf.blade.php
+++ themes/tenant/views/booklet/pdf.blade.php
@@ -37,6 +37,26 @@
             vertical-align: middle;
         }
 
+        .chip {
+            padding: 8px 10px;
+            border-radius: 25px;
+            /* font-weight: 600;
+            font-size: 12px; */
+            box-shadow: 0 2px 5px rgba(0,0,0,.25);
+            margin: 0 10px;
+            text-align: center;            
+        }
+
+        .chip.primary {
+            background: rgba(167, 243, 208, 1);
+            color: rgba(5, 150, 105, 1);
+        }
+
+        .chip.secondary {
+            background-color: rgba(254,226,226, 1);
+            color: rgba(185,28,28, 1);
+        }
+
         @page {
             /* margin: 60px 10px 66px 50px; */
         }
@@ -71,7 +91,7 @@
                     </td>
                     <td style="width: 50px;"></td>
                     <td>Nama Alat : </td>
-                    <td> {{ $inv->device->standard_name }}</td>
+                    <td colspan="3"> {{ $inv->device->standard_name }}</td>
                 </tr>
                 <tr>
                     <td style="width: 50px;"></td>
@@ -82,6 +102,17 @@
                     <td style="width: 50px;"></td>
                     <td>Ruangan : </td>
                     <td> {{ $inv->room->room_name }}</td>
+                    <td>
+                        @if ($inv->latest_record->result == "Laik")
+                            <div class="chip primary">
+                                Laik
+                            </div>
+                        @else
+                            <div class="chip secondary">
+                                Tidak Laik
+                            </div>   
+                        @endif
+                    </td>
                 </tr>
                 <tr>
                     <td style="width: 50px;"></td>
